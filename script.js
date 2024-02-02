b4006efd7796d7d536c0d3fc158d2a367f1b473d
NakamakiTetsuya <nakamaki.tetsuya@gmail.com>
2024-02-02 11:06:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ const updateUI = async () => {
     ).innerHTML = await auth0.getTokenSilently();
 
     document.getElementById("ipt-user-profile").innerHTML = JSON.stringify(
-      await auth0.getUser()
+      await auth0.getUser(), null, 4
     );
 
     //プロフ画像
